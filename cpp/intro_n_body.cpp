a3488f1e925187aaa06da9d81a48ba90eb7a3c11
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-05-24 14:55:33 +1000

    added c++ outline code
    added the code that will be used to solve an n-body problem

---
+++ cpp/intro_n_body.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+// This code is intended to be a simple introduction to the 
+// n-body problem, presented in c++. This code is not intended
+// to be a full implementation of the n-body problem, but rather
+// a simple introduction to the concepts involved and a way to
+// teach myself the language c++.
+
+
+int main(){
+    
+
+    cout << endl;
+}
+
+
+
+
+
